e99e6d1467398b56d839e16a9529f53cd29a9547
Laisla Schultz <laislaschultz@gmail.com>
2023-01-19 18:47:44 -0500

    added some HTML

--- blackjack.js
+++ blackjack.js
@@ -2,7 +2,7 @@
 // house vs player (2 player game)
 // win, lose, draw
 
-// create a deck (deck contains 52 cards)
+// create a deck (deck contains 52 cards)(13 cards per suit)
 let suits = ['spades', 'diamonds', 'clubs', 'hearts']
 let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", 'Q', "K", "A"]
 let deck = [];
@@ -40,12 +40,22 @@ function createDeck() {
 createDeck()
 //console.log(deck)
 
-// suffle deck (randomly choose 2 cards [2 for each player])
-
-
+// suffle deck (randomly choose 2 cards [2 for each player]) - googled shuffled items in an array (used The Fisher-Yates algorith - one of the options that was a bit simpler and i understood better).
+function shuffle (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+  shuffle (deck)
   //console.log(deck)
-// player is dealt 2 cards
-// house is dealt 2 cards
+
+// select a card (random)
+// player is dealt 2 cards (at random)
+// house is dealt 2 cards (at random)
+// hide one of dealers cards
 // house goes over 21 = house loses
 // player goes over 21 = player loses
 // player or house gets to 21 = wins
