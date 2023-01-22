5058e683447251a0f10b82bb0fd5d41c2307a26e
Laisla Schultz <laislaschultz@gmail.com>
2023-01-22 10:53:34 -0500

    created players function

--- blackjack.js
+++ blackjack.js
@@ -1,12 +1,11 @@
 //JS
-// house vs player (2 player game)
 
 // create a deck (deck contains 52 cards)(13 cards per suit)
 let suits = ['spades', 'diamonds', 'clubs', 'hearts']
 let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", 'Q', "K", "A"]
 let deck = [];
 
-//create a function that goes through each value and for each value, creats 4 cards, one for each suit. 
+//create a function that goes through each value and for each value, creates 4 cards, one for each suit. 
 function createDeck() {
 
      deck = [] //assigning an empty value
@@ -35,10 +34,10 @@ function createDeck() {
         }
     }
 }
-createDeck()
-//console.log(deck)
+// createDeck()
+// //console.log(deck)
 
-// suffle deck (randomly choose 2 cards [2 for each player]) - googled shuffled items in an array (used The Fisher-Yates algorith - one of the options that was a bit simpler and i understood better).
+// // suffle deck (randomly choose 2 cards [2 for each player]) - googled shuffled items in an array (used The Fisher-Yates algorith - one of the options that was a bit simpler and i understood better).
 function shuffleDeck () {
     for (let i = deck.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -47,41 +46,58 @@ function shuffleDeck () {
       deck[j] = temp;
     }
   }
-  //console.log(deck)
+//   //console.log(deck)
 
-// select a card (random)
-// player is dealt 2 cards (at random)
-// house is dealt 2 cards (at random)
-//when I click new game, cards load for both player and you at random. 
-function newGame(){
+// house vs player (2 player game) - create players
+
+let players = []
+function createPlayers(){
+
+    let player1 = {Name: "dealer", Points: 0, Hand:[]} //object
+    players.push(player1);
+
+    let player2 = {Name: 'you', Point: 0, Hand:[]}
+    players.push(player2);
+
+}
+// // select a card (random)
+// // player is dealt 2 cards (at random)
+// // house is dealt 2 cards (at random)
+// //when I click new game, cards load for both player and you at random. 
+function dealHands(){
  createDeck()
  shuffleDeck()
  let dealerHiddenCard=deck.pop()
  let dealerVisibleCard=deck.pop()
  let youFirstCard=deck.pop()
  let youSecondCard=deck.pop()
- console.log(dealerHiddenCard)
-}
-//Fetching the new-game button to JS
-let newGameButton=document.getElementById("new-game")
-
+  //console.log(dealerVisibleCard)
+ }
+// //Fetching the new-game button to JS
+ let newGameButton=document.getElementById("new-game")
 newGameButton.addEventListener("click",newGame)
 
+function dealerNewThirdCard(){
+    
+}
+let hitGameButton=document.getElementById("hit")
+hitGameButton.addEventListener("click", hit)
 
 
-// hide one of dealers cards
-// house goes over 21 = house loses
-// player goes over 21 = player loses
-// player or house gets to 21 = wins
-// both players get to 21 = draw
-// player hold higer cards than house = player wins (even if less than 21)
-// house holds higher cards than player = house wins (even if less than 21)
-// win, lose, draw
+// // hide one of dealers cards
+// // house goes over 21 = house loses
+// // player goes over 21 = player loses
+// // player or house gets to 21 = wins
+// // both players get to 21 = draw
+// // player hold higer cards than house = player wins (even if less than 21)
+// // house holds higher cards than player = house wins (even if less than 21)
+// // win, lose, draw
 
 
-//HTML
-//deck
-// house cards
-// player cards
-//add buttons for more cards (hit me or stay)
-//keep player's score
+// //HTML
+// //deck
+// // house cards
+// // player cards
+// //add buttons for more cards (hit me or stay)
+// //keep player's score
+
